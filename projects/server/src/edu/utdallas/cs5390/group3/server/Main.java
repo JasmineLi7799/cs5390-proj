266954c8e180d3656162699896cdc7e6c4bbb519
Travis Millican <travisjmillican@gmail.com>
2016-11-07 17:11:18 -0600

    Altered shut down hook registration.
    Made a minor tweak to how addShutdownHook was being called so as to
    parallel the technique used in the chat client.

--- projects/server/src/edu/utdallas/cs5390/group3/server/Main.java
+++ projects/server/src/edu/utdallas/cs5390/group3/server/Main.java
@@ -11,20 +11,11 @@ import java.util.Scanner;
 
 
 public final class Main {
-    private static Thread shutdownHandler() {
-        return new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Server.instance().shutDown();
-            }
-        });
-    }
-
     // TODO: roll System.in into Console object? Only a single thread
     // reads from System.in, so we don't need a locking semaphore as
     // with System.out...
     public static void main(String[] args) throws Exception {
-        Runtime.getRuntime().addShutdownHook(shutdownHandler());
+        Main.registerShutdownHook();
         Server server = Server.instance();
         server.spinWelcomeThread();
         Console.info("Server welcome thread running.");
@@ -58,7 +49,17 @@ public final class Main {
         // exited. Reaching the end of main() merely kills the main
         // thread (interactive console). We have to tell the various
         // other threads to die in order to terminate the server
-        // process.
+        // process. Alternatively, System.exit() will accomplish the
+        // same thing thanks to the shutdown hook.
         server.shutDown();
+    }
+
+    private static void registerShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Server.instance().shutDown();
+            }
+        }));
     }
 }
