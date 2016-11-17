7167d63cb4856f207627b252abb5fa0c96716ab5
Travis Millican <travisjmillican@gmail.com>
2016-11-17 17:26:40 -0600

    Internal documentation of Client.java

--- projects/client/src/edu/utdallas/cs5390/group3/client/Client.java
+++ projects/client/src/edu/utdallas/cs5390/group3/client/Client.java
@@ -6,7 +6,24 @@ import java.util.concurrent.Semaphore;
 
 import java.lang.InterruptedException;
 
+/* The Client class stores client information and represents the
+ * client as a state machine that changes in response to inputs from
+ * its ClientThread.
+ *
+ * The Client's state also controls whether and how other
+ * ClientThreads are permitted to interact with the Client.  For
+ * instance, when Client B attempts to send a message to Client A,
+ * Client B's ClientThread will check whether Client A is in a valid
+ * state to receive the message.
+ */
 public final class Client {
+    // Basic client info.
+    private int _id;
+    private String _privateKey;
+
+    // Client state and guard semaphore.
+    private State _state;
+    private Semaphore _stateLock;
     public static enum State {
         START,
         HELLO_SENT,
@@ -17,10 +34,26 @@ public final class Client {
         // ...
     }
 
-    private int _id;
-    private String _privateKey;
-    private State _state;
-    private Semaphore _stateLock;
+    // =========================================================================
+    // Constructor
+    // =========================================================================
+
+    /* Creates a Client in the START state.
+     *
+     * @param id Client ID
+     * @param privateKey Private key used for authentication and
+     * encryption.
+     */
+    public Client(int id, String privateKey) {
+        _id = id;
+        _privateKey = privateKey;
+        _stateLock = new Semaphore(1);
+        _state = State.START;
+    }
+
+    // =========================================================================
+    // Accessors/Mutators
+    // =========================================================================
 
     public int id() { return _id; }
     public String privateKey() { return _privateKey; }
@@ -39,13 +72,14 @@ public final class Client {
         _stateLock.release();
     }
 
-    public Client(int id, String privateKey) {
-        _id = id;
-        _privateKey = privateKey;
-        _stateLock = new Semaphore(1);
-        _state = State.START;
-    }
+    // =========================================================================
+    // Protocol messages
+    // =========================================================================
 
+    /* Generates the HELLO message payload.
+     *
+     * @return the HELLO message.
+     */
     public String hello() {
         return "HELLO " + _id;
     }
